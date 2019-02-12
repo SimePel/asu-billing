7eae88b38f47c7f3744955b81061a964529cd857
SimePel <simepeldev@gmail.com>
2019-02-12 16:47:23 +0700

    Begin authAdmin func

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func main() {
 
 	router.ServeFiles("/assets/*filepath", http.Dir("assets/"))
 	router.GET("/admin-login", adminLogin)
+	router.POST("/admin-login", authAdmin)
 
 	err := http.ListenAndServe(":8080", router)
 	if err != nil {
@@ -26,4 +27,15 @@ func main() {
 
 func adminLogin(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	t.ExecuteTemplate(w, "admin-login", nil)
+}
+
+func authAdmin(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	if err := r.ParseForm(); err != nil {
+		log.Println("form parsing: ", err)
+		http.Error(w, "Problems with fetching your data from the form. Please try again", http.StatusInternalServerError)
+		return
+	}
+
+	// ldap auth here then sql query to get the role of user
+	// if it is alright then redirect to admin page
 }
