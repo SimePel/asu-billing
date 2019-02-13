bbe3090faf07393195c3742aac843812d930cbcb
SimePel <simepeldev@gmail.com>
2019-02-13 11:29:14 +0700

    Take out admin's handlers

---
+++ admin.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+var (
+	admT = template.Must(template.New("adm").ParseGlob("templates/adm/*.html"))
+)
+
+func adminLogin(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	admT.ExecuteTemplate(w, "login", nil)
+}
+
+func authAdmin(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	if err := r.ParseForm(); err != nil {
+		log.Println("form parsing: ", err)
+		http.Error(w, "Problems with fetching your data from the form. Please try again", http.StatusInternalServerError)
+		return
+	}
+
+	// ldap auth here then sql query to get the role of user
+	// if it is alright then redirect to admin page
+}
+
+func adminIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	admT.ExecuteTemplate(w, "index", nil)
+}
--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"html/template"
 	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
-)
-
-var (
-	admT = template.Must(template.New("adm").ParseGlob("templates/adm/*.html"))
 )
 
 func main() {
@@ -18,29 +13,11 @@ func main() {
 	router.ServeFiles("/assets/*filepath", http.Dir("assets/"))
 	router.GET("/admin-login", adminLogin)
 	router.GET("/admin-index", adminIndex)
+
 	router.POST("/admin-login", authAdmin)
 
 	err := http.ListenAndServe(":8080", router)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
-}
-
-func adminLogin(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	admT.ExecuteTemplate(w, "login", nil)
-}
-
-func authAdmin(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	if err := r.ParseForm(); err != nil {
-		log.Println("form parsing: ", err)
-		http.Error(w, "Problems with fetching your data from the form. Please try again", http.StatusInternalServerError)
-		return
-	}
-
-	// ldap auth here then sql query to get the role of user
-	// if it is alright then redirect to admin page
-}
-
-func adminIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	admT.ExecuteTemplate(w, "index", nil)
 }
