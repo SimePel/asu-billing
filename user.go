00faffab4cbb70b04eff6ad9df5ba235abda0bd5
SimePel <simepeldev@gmail.com>
2019-02-15 15:37:34 +0700

    Store user data in mongodb

--- user.go
+++ user.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/mongodb/mongo-go-driver/bson"
+	"github.com/mongodb/mongo-go-driver/mongo"
 	ldap "gopkg.in/ldap.v3"
 )
 
@@ -16,13 +20,71 @@ var (
 	usrT = template.Must(template.New("usr").ParseGlob("templates/usr/*.html"))
 )
 
+// User is data type from mongodb
+type User struct {
+	Name         string    `bson:"name"`
+	Money        int       `bson:"money"`
+	Login        string    `bson:"login"`
+	Tariff       int       `bson:"tariff"`
+	InIP         string    `bson:"in_ip"`
+	ExtIP        string    `bson:"ext_ip"`
+	PaymentsEnds time.Time `bson:"payments_ends,omitempty"`
+}
+
+// CorrectedUser needs to appropriate printing on user page
+type CorrectedUser struct {
+	Money        int
+	Name         string
+	Login        string
+	Tariff       string
+	InIP         string
+	ExtIP        string
+	PaymentsEnds string
+}
+
 func userIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	session, _ := store.Get(r, "user")
-	if session.Values["user_logged"] == "true" {
-		usrT.ExecuteTemplate(w, "index", nil)
+	if session.Values["user_logged"] == "false" || session.Values["user_logged"] == nil {
+		http.Redirect(w, r, "/user-login", http.StatusFound)
 		return
 	}
-	http.Redirect(w, r, "/user-login", http.StatusFound)
+
+	client, err := mongo.Connect(context.Background(), "mongodb://localhost:27017")
+	if err != nil {
+		log.Fatal("could not connect to mongo", err)
+	}
+
+	user := User{}
+	flashes := session.Flashes()
+	filter := bson.M{"login": flashes[len(flashes)-1].(string)}
+	coll := client.Database("billing").Collection("users")
+	err = coll.FindOne(context.Background(), filter).Decode(&user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	usrT.ExecuteTemplate(w, "index", CorrectedUser{
+		Tariff:       tariffStringRepr(user.Tariff),
+		Money:        user.Money,
+		Name:         user.Name,
+		Login:        user.Login,
+		InIP:         user.InIP,
+		ExtIP:        user.ExtIP,
+		PaymentsEnds: user.PaymentsEnds.Format("2.01.2006"),
+	})
+}
+
+func tariffStringRepr(tariff int) string {
+	if tariff == 1 {
+		return "Базовый30мб (300р)"
+	}
+	if tariff == 2 {
+		return "Базовый30мб+IP (400р)"
+	}
+	if tariff == 3 {
+		return "Беспроводной (200р)"
+	}
+	return "Неизвестный тариф"
 }
 
 func userLogin(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
@@ -91,8 +153,19 @@ func authUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	l.Close()
 
 	session.Values["user_logged"] = "true"
+	session.AddFlash(pieces[1] + getRightPostfix(pieces[0]))
 	session.Save(r, w)
 	http.Redirect(w, r, "/user", http.StatusFound)
+}
+
+func getRightPostfix(domain string) string {
+	if domain == "stud" {
+		return "@stud.asu.ru"
+	}
+	if domain == "mc" {
+		return "@mc.asu.ru"
+	}
+	return "Неизвестный домен"
 }
 
 func userLogout(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
