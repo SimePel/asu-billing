a6a36f4dfeef4135e57d049bfae7a8cda9cb25b7
SimePel <simepeldev@gmail.com>
2019-02-28 13:06:53 +0700

    Remove explicit links on admin page

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,4 +1,18 @@
 window.onload = function () {
+    url = new URL(window.location.href);
+    type = url.searchParams.get("type");
+    if (type === "wired") {
+        document.getElementById("wired").innerHTML = "<strong>Проводные</strong/";
+    } else if (type === "wireless") {
+        document.getElementById("wireless").innerHTML = "<strong>Беспроводные</strong/";
+    } else if (type === "active") {
+        document.getElementById("active").innerHTML = "<strong>Включенные</strong/";
+    } else if (type === "inactive") {
+        document.getElementById("inactive").innerHTML = "<strong>Отключенные</strong/";
+    } else {
+        document.getElementById("all").innerHTML = "<strong>Все</strong/";
+    }
+
     const searchButton = document.getElementById("searchButton")
 
     searchButton.addEventListener('click', function (event) {
