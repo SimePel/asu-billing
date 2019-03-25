e991830d8e9140ede11e6a4e36efb599c94a73a6
SimePel <simepeldev@gmail.com>
2019-03-25 16:02:17 +0700

    Show appropriate rows by type on admin page

--- mysql.go
+++ mysql.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"time"
 )
@@ -122,12 +123,7 @@ func setInIPAsUsed(inIP string) error {
 }
 
 func getUsersByType(t string) ([]User, error) {
-	rows, err := db.Query(`SELECT Users.ID, Users.Name, Users.Login, Users.Money, Users.Active, Users.Phone,
-		Users.Comment, Users.Payments_ends, In_IPs.IP, Ext_IPs.IP, Tariffs.ID, Tariffs.Name, Tariffs.Price
-	FROM (((Users
-		INNER JOIN In_IPs ON Users.In_IP_ID = In_IPs.ID)
-		INNER JOIN Ext_IPs ON Users.Ext_IP_ID = Ext_IPs.ID)
-		INNER JOIN Tariffs ON Users.Tariff_ID = Tariffs.ID)`)
+	rows, err := getRowsByType(t)
 	if err != nil {
 		return nil, fmt.Errorf("could not do query: %v", err)
 	}
@@ -149,6 +145,51 @@ func getUsersByType(t string) ([]User, error) {
 	}
 
 	return users, nil
+}
+
+func getRowsByType(t string) (*sql.Rows, error) {
+	if t == "wired" {
+		return db.Query(`SELECT Users.ID, Users.Name, Users.Login, Users.Money, Users.Active, Users.Phone,
+		Users.Comment, Users.Payments_ends, In_IPs.IP, Ext_IPs.IP, Tariffs.ID, Tariffs.Name, Tariffs.Price
+		FROM (((Users
+			INNER JOIN In_IPs ON Users.In_IP_ID = In_IPs.ID)
+			INNER JOIN Ext_IPs ON Users.Ext_IP_ID = Ext_IPs.ID)
+			INNER JOIN Tariffs ON Users.Tariff_ID = Tariffs.ID)
+		WHERE Tariff_ID = 1 OR Tariff_ID = 2`)
+	}
+	if t == "wireless" {
+		return db.Query(`SELECT Users.ID, Users.Name, Users.Login, Users.Money, Users.Active, Users.Phone,
+		Users.Comment, Users.Payments_ends, In_IPs.IP, Ext_IPs.IP, Tariffs.ID, Tariffs.Name, Tariffs.Price
+		FROM (((Users
+			INNER JOIN In_IPs ON Users.In_IP_ID = In_IPs.ID)
+			INNER JOIN Ext_IPs ON Users.Ext_IP_ID = Ext_IPs.ID)
+			INNER JOIN Tariffs ON Users.Tariff_ID = Tariffs.ID)
+		WHERE Tariff_ID = 3`)
+	}
+	if t == "active" {
+		return db.Query(`SELECT Users.ID, Users.Name, Users.Login, Users.Money, Users.Active, Users.Phone,
+		Users.Comment, Users.Payments_ends, In_IPs.IP, Ext_IPs.IP, Tariffs.ID, Tariffs.Name, Tariffs.Price
+		FROM (((Users
+			INNER JOIN In_IPs ON Users.In_IP_ID = In_IPs.ID)
+			INNER JOIN Ext_IPs ON Users.Ext_IP_ID = Ext_IPs.ID)
+			INNER JOIN Tariffs ON Users.Tariff_ID = Tariffs.ID)
+		WHERE Active = 1`)
+	}
+	if t == "inactive" {
+		return db.Query(`SELECT Users.ID, Users.Name, Users.Login, Users.Money, Users.Active, Users.Phone,
+		Users.Comment, Users.Payments_ends, In_IPs.IP, Ext_IPs.IP, Tariffs.ID, Tariffs.Name, Tariffs.Price
+		FROM (((Users
+			INNER JOIN In_IPs ON Users.In_IP_ID = In_IPs.ID)
+			INNER JOIN Ext_IPs ON Users.Ext_IP_ID = Ext_IPs.ID)
+			INNER JOIN Tariffs ON Users.Tariff_ID = Tariffs.ID)
+		WHERE Active = 0`)
+	}
+	return db.Query(`SELECT Users.ID, Users.Name, Users.Login, Users.Money, Users.Active, Users.Phone,
+	Users.Comment, Users.Payments_ends, In_IPs.IP, Ext_IPs.IP, Tariffs.ID, Tariffs.Name, Tariffs.Price
+	FROM (((Users
+		INNER JOIN In_IPs ON Users.In_IP_ID = In_IPs.ID)
+		INNER JOIN Ext_IPs ON Users.Ext_IP_ID = Ext_IPs.ID)
+		INNER JOIN Tariffs ON Users.Tariff_ID = Tariffs.ID)`)
 }
 
 func getUserByID(id int) (User, error) {
