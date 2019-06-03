7b883c3eb8e4532793cffbdaa055a04176b244bf
SimePel <simepeldev@gmail.com>
2019-06-03 09:20:50 +0700

    Refactoring

--- user.go
+++ user.go
@@ -8,22 +8,23 @@ import (
 	"github.com/go-chi/chi"
 )
 
+type userCtxKey string
+
 func userCtx(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		var user *User
-		if userID := chi.URLParam(r, "userID"); userID != "" {
-			user = dbGetUser(userID)
-		} else {
+		userID := chi.URLParam(r, "userID")
+		if userID == "" {
 			http.Error(w, http.StatusText(404), 404)
 			return
 		}
 
-		ctx := context.WithValue(r.Context(), "user", user)
+		user := dbGetUser(userID)
+		ctx := context.WithValue(r.Context(), userCtxKey("user"), user)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
 
 func getUser(w http.ResponseWriter, r *http.Request) {
-	user := r.Context().Value("user").(*User)
+	user := r.Context().Value(userCtxKey("user")).(*User)
 	json.NewEncoder(w).Encode(user)
 }
