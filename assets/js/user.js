fa5df79eb1f53209c54d3183688e3129331482bb
SimePel <simepeldev@gmail.com>
2019-06-06 14:46:09 +0700

    Get user data by id from jwt token now

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,6 +1,19 @@
-fetch("users/1").then((res) => { return res.json() }).then((data) => {
-    document.getElementById("name").innerHTML = data.name;
-    document.getElementById("login").innerHTML = data.login;
-    document.getElementById("balance").innerHTML = data.balance;
-    document.getElementById("agreement").innerHTML = data.agreement;
-})
+{
+    let tokenStr = document.cookie.split(";").filter((item) => item.includes("jwt"))[0].split("=")[1]
+    let t = parseJWT(tokenStr)
+    fetch("users/" + t.id).then((res) => { return res.json() }).then((data) => {
+        document.getElementById("name").innerHTML = data.name;
+        document.getElementById("login").innerHTML = data.login;
+        document.getElementById("balance").innerHTML = data.balance;
+        document.getElementById("agreement").innerHTML = data.agreement;
+    })
+}
+
+function parseJWT(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = decodeURIComponent(atob(base64Url).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(base64);
+};
