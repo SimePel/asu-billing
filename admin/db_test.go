71be6c2ee8ca989c9bae89a851df4cab31d9278c
SimePel <simepeldev@gmail.com>
2019-09-03 16:10:53 +0700

    Make test - getUserById without mocking

--- admin/db_test.go
+++ admin/db_test.go
@@ -208,40 +208,25 @@ func TestGetAllUsers(t *testing.T) {
 }
 
 func TestGetUserByID(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	require.NoError(t, err)
-	defer db.Close()
-
 	expectedUser := User{
-		ID:              1,
-		Activity:        false,
-		Name:            "Тест",
-		Agreement:       "П-777",
-		Room:            "502",
-		Phone:           "88005553550",
-		Login:           "blabla.123",
-		InnerIP:         "10.80.80.1",
-		ExtIP:           "82.200.46.10",
-		Balance:         0,
-		ConnectionPlace: "Не важно",
-		ExpiredDate:     time.Now().Add(time.Minute),
+		ID:          1,
+		Activity:    true,
+		Name:        "Тестовый Тест Тестович",
+		Agreement:   "П-001",
+		Phone:       "88005553550",
+		Login:       "blabla.123",
+		InnerIP:     "10.1.108.1",
+		ExtIP:       "82.200.46.10",
+		Balance:     100,
+		ExpiredDate: time.Date(2019, time.June, 27, 4, 25, 26, 0, time.UTC),
 		Tariff: Tariff{
 			ID:    1,
-			Name:  "Проводной",
+			Name:  "Базовый-30",
 			Price: 200,
 		},
 	}
 
-	rows := sqlmock.NewRows([]string{"id", "balance", "name", "login", "agreement", "expired_date",
-		"connection_place", "activity", "room", "phone", "tariff_id", "tariff_name", "price", "ip", "ext_ip"}).
-		AddRow(expectedUser.ID, expectedUser.Balance, expectedUser.Name, expectedUser.Login,
-			expectedUser.Agreement, expectedUser.ExpiredDate, expectedUser.ConnectionPlace,
-			expectedUser.Activity, expectedUser.Room, expectedUser.Phone, expectedUser.Tariff.ID,
-			expectedUser.Tariff.Name, expectedUser.Tariff.Price, expectedUser.InnerIP, expectedUser.ExtIP)
-
-	mock.ExpectQuery("SELECT (.+) FROM (.+) WHERE users.id = ").WithArgs(expectedUser.ID).WillReturnRows(rows)
-
-	actualUser, err := GetUserByID(db, int(expectedUser.ID))
+	actualUser, err := GetUserByID(mysql.db, int(expectedUser.ID))
 	require.NoError(t, err)
 	assert.Equal(t, expectedUser, actualUser)
 }
