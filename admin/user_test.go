534815c1c03a341ab153a8f4941ec4e40d9570ca
SimePel <simepeldev@gmail.com>
2019-09-12 13:47:34 +0700

    Make TestGetAllUsersHandler without mocking

--- admin/user_test.go
+++ admin/user_test.go
@@ -7,9 +7,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
 
-	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/go-chi/chi"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -97,66 +95,6 @@ func TestGetUser(t *testing.T) {
 }
 
 func TestGetAllUsersHandler(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	require.NoError(t, err)
-	defer db.Close()
-
-	expectedUsers := []User{
-		{
-			ID:              1,
-			Paid:            false,
-			Activity:        false,
-			Name:            "Тест",
-			Agreement:       "П-777",
-			Room:            "502",
-			Phone:           "88005553550",
-			Login:           "blabla.123",
-			InnerIP:         "10.80.80.1",
-			ExtIP:           "82.200.46.10",
-			Balance:         0,
-			ConnectionPlace: "Не важно",
-			ExpiredDate:     time.Now().Add(time.Minute).Local(),
-			Tariff: Tariff{
-				ID:    1,
-				Name:  "Проводной",
-				Price: 200,
-			}},
-		{
-			ID:              2,
-			Paid:            false,
-			Activity:        false,
-			Name:            "Тест2",
-			Agreement:       "П-123",
-			Room:            "501",
-			Phone:           "88005553551",
-			Login:           "bla.124",
-			InnerIP:         "10.80.80.2",
-			ExtIP:           "82.200.46.10",
-			Balance:         0,
-			ConnectionPlace: "Не важно",
-			ExpiredDate:     time.Now().Add(time.Minute).Local(),
-			Tariff: Tariff{
-				ID:    1,
-				Name:  "Проводной",
-				Price: 200,
-			}},
-	}
-
-	rows := sqlmock.NewRows([]string{"id", "balance", "name", "login", "agreement", "expired_date", "connection_place",
-		"paid", "activity", "room", "phone", "tariff_id", "tariff_name", "price", "ip", "ext_ip"}).
-		AddRow(expectedUsers[0].ID, expectedUsers[0].Balance, expectedUsers[0].Name, expectedUsers[0].Login,
-			expectedUsers[0].Agreement, expectedUsers[0].ExpiredDate, expectedUsers[0].ConnectionPlace,
-			expectedUsers[0].Paid, expectedUsers[0].Activity, expectedUsers[0].Room, expectedUsers[0].Phone,
-			expectedUsers[0].Tariff.ID, expectedUsers[0].Tariff.Name, expectedUsers[0].Tariff.Price,
-			expectedUsers[0].InnerIP, expectedUsers[0].ExtIP).
-		AddRow(expectedUsers[1].ID, expectedUsers[1].Balance, expectedUsers[1].Name, expectedUsers[1].Login,
-			expectedUsers[1].Agreement, expectedUsers[1].ExpiredDate, expectedUsers[1].ConnectionPlace,
-			expectedUsers[1].Paid, expectedUsers[1].Activity, expectedUsers[1].Room, expectedUsers[1].Phone,
-			expectedUsers[1].Tariff.ID, expectedUsers[1].Tariff.Name, expectedUsers[1].Tariff.Price,
-			expectedUsers[1].InnerIP, expectedUsers[1].ExtIP)
-
-	mock.ExpectQuery("SELECT (.+) FROM (.+)").WillReturnRows(rows)
-
 	req, err := http.NewRequest("GET", "/users/", nil)
 	require.Nil(t, err)
 
@@ -164,8 +102,9 @@ func TestGetAllUsersHandler(t *testing.T) {
 		getAllUsers(w, r)
 	})
 
+	mysql := MySQL{db: openTestDBconnection()}
 	ctx := req.Context()
-	ctx = context.WithValue(ctx, dbCtxKey("db"), db)
+	ctx = context.WithValue(ctx, dbCtxKey("db"), mysql.db)
 	req = req.WithContext(ctx)
 	rr := httptest.NewRecorder()
 	handler.ServeHTTP(rr, req)
@@ -175,5 +114,5 @@ func TestGetAllUsersHandler(t *testing.T) {
 	require.Nil(t, err)
 
 	assert.Equal(t, 200, rr.Code)
-	assert.Equal(t, expectedUsers, actualUsers)
+	assert.NotNil(t, actualUsers)
 }
