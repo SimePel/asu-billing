3a38867428c97cdfea33a3e0d228826d5a1c2819
SimePel <simepeldev@gmail.com>
2019-09-18 15:12:14 +0700

    Display stats in another way

--- admin/assets/js/index.js
+++ admin/assets/js/index.js
@@ -120,10 +120,8 @@ function showStatistics() {
     fetch("stats").then(res => {
         return res.json();
     }).then(stats => {
-        document.querySelector("#countOfAllUsers").textContent = stats.active_users_count + stats.inactive_users_count;
         document.querySelector("#countOfActiveUsers").textContent = stats.active_users_count;
         document.querySelector("#countOfInactiveUsers").textContent = stats.inactive_users_count;
-        document.querySelector("#allMoney").textContent = stats.all_money;
     });
 }
 
