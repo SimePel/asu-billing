61b12e393df00616ea6fe2442c74dd6eac0decd6
SimePel <simepeldev@gmail.com>
2019-09-18 10:42:07 +0700

    Add restorePaymentsTimers func

--- admin/main.go
+++ admin/main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
+	err := restorePaymentsTimers()
+	if err != nil {
+		log.Fatal(err)
+	}
 	r := newRouter()
 	log.Fatal(http.ListenAndServe(":8081", r))
+}
+
+func restorePaymentsTimers() error {
+	mysql := MySQL{db: initializeDB()}
+	users, err := mysql.GetAllUsers()
+	if err != nil {
+		return fmt.Errorf("cannot get all users: %v", err)
+	}
+
+	for _, user := range users {
+		if user.ExpiredDate.After(time.Now()) {
+			expirationDate := time.Until(user.ExpiredDate)
+			f := createTryToRenewPaymentFunc(mysql, user)
+			time.AfterFunc(expirationDate, f)
+			continue
+		}
+		tryToRenewPayment(mysql, user)
+	}
+
+	return nil
 }
