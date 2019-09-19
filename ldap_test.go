94e25aeffc06c99d8726ca944ae1cb40f8feb9f9
SimePel <simepeldev@gmail.com>
2019-09-19 10:24:57 +0700

    Improve ldapAuth test

--- ldap_test.go
+++ ldap_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestLDAPauth(t *testing.T) {
@@ -20,4 +21,14 @@ func TestLDAPauth(t *testing.T) {
 	password := os.Getenv("LDAP_TEST_PASSWORD")
 	err = ldapAuth(login, password)
 	assert.Nil(t, err)
+
+	bindUsername := os.Getenv("LDAP_LOGIN")
+	err = os.Setenv("LDAP_LOGIN", "a")
+	require.NoError(t, err)
+
+	err = ldapAuth("l", "p")
+	require.Error(t, err)
+
+	err = os.Setenv("LDAP_LOGIN", bindUsername)
+	require.NoError(t, err)
 }
