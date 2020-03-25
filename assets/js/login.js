e1c295ce76cdedf174131ad52a7212578d71fbfc
Sime_Pel <simepeldev@gmail.com>
2020-03-25 11:51:36 +0700

    Prettier

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,30 +1,30 @@
 function auth() {
-    fetch("login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json; charset=utf-8",
-        },
-        body: JSON.stringify({
-            login: document.getElementById("login").value,
-            password: document.getElementById("password").value,
-        }),
+  fetch("login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+    },
+    body: JSON.stringify({
+      login: document.getElementById("login").value,
+      password: document.getElementById("password").value,
+    }),
+  })
+    .then(res => {
+      return res.json();
     })
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            if (data.answer != "ok") {
-                document.getElementById("errorHere").innerHTML = data.error;
-                return;
-            }
+    .then(data => {
+      if (data.answer != "ok") {
+        document.getElementById("errorHere").innerHTML = data.error;
+        return;
+      }
 
-            window.location.replace("/");
-        });
+      window.location.replace("/");
+    });
 }
 
 document.getElementById("loginBtn").addEventListener("click", auth);
 document.getElementById("password").addEventListener("keyup", event => {
-    if (event.keyCode === 13) {
-        auth();
-    }
+  if (event.keyCode === 13) {
+    auth();
+  }
 });
